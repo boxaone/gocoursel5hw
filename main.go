74fe568125466561fe241656cd41606dd63ea16f
Oleksii Pysarenko <boxaone@outlook.com>
2022-06-15 02:23:30 +0300

    Adding Hrn type as float64

--- main.go
+++ main.go
@@ -2,22 +2,23 @@ package main
 
 import "fmt"
 
-type Cop int32 // Кількість цілих копійок
+type Cop int32   // Кількість цілих копійок
+type Hrn float64 // Кількість цілих копійок
 
 const (
-	ApplePrice = 5.99  // Ціна яблук
-	PearPrice  = 7.00  // Ціна груш
-	WholeMoney = 23.00 // Грошей взагалі в наявності
+	ApplePrice Hrn = 5.99  // Ціна яблук
+	PearPrice  Hrn = 7.00  // Ціна груш
+	WholeMoney Hrn = 23.00 // Грошей взагалі в наявності
 )
 
-// Переводимо гривні(раціональні float64) в копійки(цілі)
-func fToCop(grn float64) Cop {
+// Переводимо гривні(раціональні) в копійки(цілі)
+func hrnToCop(grn Hrn) Cop {
 	return (Cop)(grn * 100)
 }
 
-// Переводимо копійки(цілі) в гривні(раціональні float64)
-func copToF(cop Cop) float64 {
-	return (float64)(cop) / 100
+// Переводимо копійки(цілі) в гривні(раціональні)
+func copToHrn(cop Cop) Hrn {
+	return (Hrn)(cop) / 100
 }
 
 // Виводимо boolean українською
@@ -34,10 +35,10 @@ func main() {
 
 	fmt.Printf("Ми маємо: %v грн взагалі\n", WholeMoney)
 	fmt.Println("Скільки грошей треба витратити, щоб купити 9 яблук та 8 груш?")
-	fmt.Printf("Щоб купити 9 яблук та 8 груш треба %v грн\n", copToF(9*fToCop(ApplePrice)+8*fToCop(PearPrice)))
+	fmt.Printf("Щоб купити 9 яблук та 8 груш треба %v грн\n", copToHrn(9*hrnToCop(ApplePrice)+8*hrnToCop(PearPrice)))
 	fmt.Println("Скільки груш ми можемо купити?")
-	fmt.Printf("Ми можемо купити %v груш\n", fToCop(WholeMoney)/fToCop(PearPrice))
+	fmt.Printf("Ми можемо купити %v груш\n", hrnToCop(WholeMoney)/hrnToCop(PearPrice))
 	fmt.Println("Скільки яблук ми можемо купити?")
-	fmt.Printf("Ми можемо купити %v яблук\n", fToCop(WholeMoney)/fToCop(ApplePrice))
-	fmt.Printf("Чи ми можемо купити 2 груші та 2 яблука? %s!\n", boolToUkrStr(fToCop(WholeMoney) > 2*fToCop(PearPrice)+2*fToCop(ApplePrice)))
+	fmt.Printf("Ми можемо купити %v яблук\n", hrnToCop(WholeMoney)/hrnToCop(ApplePrice))
+	fmt.Printf("Чи ми можемо купити 2 груші та 2 яблука? %s!\n", boolToUkrStr(hrnToCop(WholeMoney) > 2*hrnToCop(PearPrice)+2*hrnToCop(ApplePrice)))
 }
