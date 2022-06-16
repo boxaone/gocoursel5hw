e4da5bc5891977a3faa62b6c1c16c81cbc5bdf45
Oleksii Pysarenko <boxaone@outlook.com>
2022-06-16 15:13:04 +0300

    More unification

--- main.go
+++ main.go
@@ -16,8 +16,10 @@ const (
 )
 
 var (
-	buy1 = Buy{Apples: 9, Pears: 8}
-	buy4 = Buy{Pears: 2, Apples: 2}
+	buy1 = &Buy{Apples: 9, Pears: 8}
+	buy2 = &Buy{Pears: int(hrnToCop(WholeMoney) / hrnToCop(PearPrice))}
+	buy3 = &Buy{Apples: int(hrnToCop(WholeMoney) / hrnToCop(ApplePrice))}
+	buy4 = &Buy{Pears: 2, Apples: 2}
 )
 
 // Переводимо гривні(раціональні) в копійки(цілі)
@@ -46,14 +48,30 @@ func boolToUkrStr(value bool) string {
 	return "Ні"
 }
 
+func buyToS(buy *Buy) string {
+	var s string
+	if buy.Apples > 0 {
+		s = s + fmt.Sprintf("%v яблук", buy.Apples)
+	}
+
+	if buy.Pears > 0 {
+		if buy.Apples > 0 {
+			s = s + " та "
+		}
+		s = s + fmt.Sprintf("%v груш", buy.Pears)
+	}
+	return s
+}
+
 func main() {
 
 	fmt.Printf("Ми маємо: %v грн взагалі\n", WholeMoney)
-	fmt.Printf("Скільки грошей треба витратити, щоб купити %v яблук та %v груш?", buy1.Apples, buy1.Pears)
-	fmt.Printf("Щоб купити %v яблук та %v груш треба %v грн\n", buy1.Apples, buy1.Pears, buySum(&buy1))
+	fmt.Printf("Скільки грошей треба витратити, щоб купити %s?\n", buyToS(buy1))
+	fmt.Printf("Щоб купити %s треба %v грн\n", buyToS(buy1), buySum(buy1))
 	fmt.Println("Скільки груш ми можемо купити?")
-	fmt.Printf("Ми можемо купити %v груш\n", hrnToCop(WholeMoney)/hrnToCop(PearPrice))
+	fmt.Printf("Ми можемо купити %s\n", buyToS(buy2))
 	fmt.Println("Скільки яблук ми можемо купити?")
-	fmt.Printf("Ми можемо купити %v яблук\n", hrnToCop(WholeMoney)/hrnToCop(ApplePrice))
-	fmt.Printf("Чи ми можемо купити %v груші та %v яблука? %s!\n", buy4.Pears, buy4.Apples, boolToUkrStr(WholeMoney >= buySum(&buy4)))
+	fmt.Printf("Ми можемо купити %s\n", buyToS(buy3))
+	fmt.Printf("Чи ми можемо купити %s? %s!\n", buyToS(buy4), boolToUkrStr(WholeMoney >= buySum(buy4)))
+
 }
